5fadc808fc6d4f5cc11e50a1213c14ae5c7eae24
woodenToaster <litmusik@yahoo.com>
2014-10-20 18:52:54 -0500

    Able to print Augmented grammar

--- LR0Items.py
+++ LR0Items.py
@@ -10,11 +10,18 @@ class LR0Items:
 
     def __init__(self):
         self.productions = [x.strip() for x in sys.stdin]
-        self.aug_productions = [("'", "->%s" % self.productions[0])]
+        self.aug_productions = [("'", "%s" % self.productions[0])]
         regex = re.compile(r'([A-Z])->(.*)')
         for p in self.productions[1:]:
             m = regex.match(p)
             self.aug_productions.append((m.group(1), m.group(2)))
 
+    def printAugmentedGrammar(self):
+        print("\nAugmented Grammar\n-----------------")
+        for tup in self.aug_productions:
+            print("%s->%s" % (tup[0], tup[1]))
+
+
 x = LR0Items()
-print(x.aug_productions)
+x.printAugmentedGrammar()
+print("\nSets of LR(0) Items\n-------------------")
--- TestLR0Items.py
+++ TestLR0Items.py
@@ -3,7 +3,20 @@ import re
 import LR0Items
 
 class TestLR0Items(unittest.TestCase):
-    
+
+    def setUp(self):
+        self.aug_productions = [
+            ("'", '->E'), 
+            ('E', 'E+T'), 
+            ('E', 'T'), 
+            ('T', 'T*F'), 
+            ('T', 'F'), 
+            ('F', '(E)'), 
+            ('F', 'i')
+        ]
+    def testProductionsProperlyInitialized(self):
+        self.assertEqual(self.aug_productions[1][1], 'E+T')
+        self.assertEqual(len(self.aug_productions), 7)
 
 if __name__ == '__main__':
 	unittest.main()
